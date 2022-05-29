b9d65f99c235e06ff5414f22c5b80f5e35acebea
Essa Sheikh <essaubaid0@gmail.com>
2022-05-29 21:34:54 +0500

    again

--- models/User.js
+++ models/User.js
@@ -3,28 +3,31 @@ const { required } = require('nodemon/lib/config');
 
 const UserSchema = new mongoose.Schema({
 
-    username : { type: String, required: true, unique:true},
-    email: {type: String, required: true, unique: true },
-    password: {type: String, required: true },
-    f_name: {type: String, pattern: "^[+][0-9]*$", required: true},
-    l_name: {type: String, required: true},
-    phone_number: {type: Object, properties: {
-        areaCode:{type: String, pattern: "^[+][0-9]*$",minLength:3, maxLength:4},
-        number:{type:String, pattern: "^[0-9]$", minLength:10, maxLength:10},
-        },
-        required:["areaCode", "number"], additionalProperties: false,
-    },
-    billing_address:{type: Object, properties:{
-        street_address: {type: String, maxlength:100},
-        city: {type: String, maxlength: 20},
-        country: {type:String, maxlength:20},
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    f_name: { type: String, pattern: "^[+][0-9]*$", required: true },
+    l_name: { type: String, required: true },
+    phone_number: {
+        type: Object, properties: {
+            areaCode: { type: String, pattern: "^[+][0-9]*$", minLength: 3, maxLength: 4 },
+            number: { type: String, pattern: "^[0-9]$", minLength: 10, maxLength: 10 },
         },
         additionalProperties: false,
     },
-    shipping_address:{type: Object, properties:{
-        street_address: {type: String, maxlength:100},
-        city: {type: String, maxlength: 20},
-        country: {type:String, maxlength:20},
+    billing_address: {
+        type: Object, properties: {
+            street_address: { type: String, maxlength: 100 },
+            city: { type: String, maxlength: 20 },
+            country: { type: String, maxlength: 20 },
+        },
+        additionalProperties: false,
+    },
+    shipping_address: {
+        type: Object, properties: {
+            street_address: { type: String, maxlength: 100 },
+            city: { type: String, maxlength: 20 },
+            country: { type: String, maxlength: 20 },
         },
         additionalProperties: false,
     },
@@ -34,9 +37,9 @@ const UserSchema = new mongoose.Schema({
         default: "USER",
     },
     token: {
-        type:String,
+        type: String,
     },
-}, {timestamps: true}
+}, { timestamps: true }
 );
 
 module.exports = mongoose.model("User", UserSchema);
