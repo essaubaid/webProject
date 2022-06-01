b948eb11624f8e553f592205b0b8e17ac8466c74
essaubaid <essaubaid0@gmail.com>
2022-06-01 20:07:03 +0500

    git resolve

--- routes/stripe.js
+++ routes/stripe.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 
-<<<<<<< HEAD
 router.post("/payment", (req, res) => {
     try {
         stripe.charges.create({
@@ -15,18 +14,6 @@ router.post("/payment", (req, res) => {
             } else {
                 res.status(200).json(stripeRes);
             }
-=======
-router.post("/payment" , (req,res) =>{
-    stripe.charges.create({
-        source:req.body.tokenID,
-        amount:req.body.amount,
-        currency:"usd",
-    }, (stripeErr, stripeRes)=>{
-        if(stripeErr){
-            res.status(500).json(stripeErr);
-        } else {
-            res.status(200).json(stripeRes);
->>>>>>> 0650c29fed3eda3b0994d626dada9c82fd4f1200
         }
 
         )
