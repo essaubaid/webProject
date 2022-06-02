afe35b9747b8d1bb4a6e63bb566320fc3108148d
essaubaid <essaubaid0@gmail.com>
2022-06-02 16:56:16 +0500

    images error again updated

--- routes/images.js
+++ routes/images.js
@@ -7,13 +7,11 @@ const router = require("express").Router();
 router.get("/:key", async (req, res) => {
 
     const key = req.params.key
-    if (key) {
+    if (!(key === "undefined")) {
         const readStream = getFileStream(key)
         readStream.pipe(res)
     }
-    else {
-
-    }
+    else { }
 
 });
 
